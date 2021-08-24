8645fc3eb6a35851c4360ec2620df55e9925c71a
Susmit Singh <singhsus25mit@gmail.com>
2021-08-24 11:24:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+const PORT = 3456;
+
+app.use((req, res) => {
+  const { headers, method, query, url } = req;
+  const responseBody = { headers, method, query, url, data };
+  res.json(responseBody);
+});
+
+app.listen(PORT, () => {
+  console.log(`[INFO] Server now listening on port: ${PORT}`);
+});
